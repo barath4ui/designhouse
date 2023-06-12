5fcf876cb50087b7fa452b05cd20fa5d1c3e770d
Barathkumar Ramadoss <barathkumarr@vuram.com>
2023-06-12 17:47:10 +0530

    Updates

---
+++ precache-manifest.fd914068073e25284b580efb86751a31.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "e487abebc12dcb18ed3e",
+    "url": "/designhouse/static/css/main.bb7ce9d5.chunk.css"
+  },
+  {
+    "revision": "e487abebc12dcb18ed3e",
+    "url": "/designhouse/static/js/main.e487abeb.chunk.js"
+  },
+  {
+    "revision": "ec5ddaa9f6f0a593122a",
+    "url": "/designhouse/static/js/1.ec5ddaa9.chunk.js"
+  },
+  {
+    "revision": "a44d98bbf19e0b2248ff",
+    "url": "/designhouse/static/js/2.a44d98bb.chunk.js"
+  },
+  {
+    "revision": "e7eb043846ef41c1fbe2",
+    "url": "/designhouse/static/js/runtime~main.e7eb0438.js"
+  },
+  {
+    "revision": "0b45c45e7380f108bfcaaf63b43e0901",
+    "url": "/designhouse/static/media/service-1.0b45c45e.png"
+  },
+  {
+    "revision": "2365632ff9669ecfa2ffb8967a43a449",
+    "url": "/designhouse/static/media/clients-1.2365632f.jpg"
+  },
+  {
+    "revision": "418af391442926bc4390b84816cc6e56",
+    "url": "/designhouse/static/media/j-img.418af391.png"
+  },
+  {
+    "revision": "005f08a067ae54e5ce2bb1afb53865f7",
+    "url": "/designhouse/static/media/j-img2.005f08a0.png"
+  },
+  {
+    "revision": "9da88a7e3d80443180f859b1a59df50d",
+    "url": "/designhouse/static/media/j-img1.9da88a7e.png"
+  },
+  {
+    "revision": "16bd9b48600e31c85c7cbdff95d643cb",
+    "url": "/designhouse/static/media/j-banner.16bd9b48.png"
+  },
+  {
+    "revision": "41556471d990df32bfff8a7d7f8ea202",
+    "url": "/designhouse/static/media/j-banner2.41556471.png"
+  },
+  {
+    "revision": "244d36a5c022dcaa1cf5d46d968d1d6f",
+    "url": "/designhouse/static/media/work1.244d36a5.png"
+  },
+  {
+    "revision": "47e1268f9cc8de249f808d773f4b2201",
+    "url": "/designhouse/static/media/work2.47e1268f.png"
+  },
+  {
+    "revision": "2b722a8c693060be19efc2352f947e61",
+    "url": "/designhouse/static/media/work3.2b722a8c.png"
+  },
+  {
+    "revision": "edadde51e1cdf27f424d84156d65adcc",
+    "url": "/designhouse/static/media/stories-1.edadde51.png"
+  },
+  {
+    "revision": "8db4af4e4ce551138ab359062bf511ff",
+    "url": "/designhouse/static/media/stories-2.8db4af4e.png"
+  },
+  {
+    "revision": "82dbca5d63960315f70a7a122568a59e",
+    "url": "/designhouse/static/media/stories-3.82dbca5d.png"
+  },
+  {
+    "revision": "216e1a34964794eb87a9157f6e2ed591",
+    "url": "/designhouse/static/media/clients-2.216e1a34.jpg"
+  },
+  {
+    "revision": "5db1d8e82a1331fef532c2efc1453216",
+    "url": "/designhouse/static/media/w-banner.5db1d8e8.png"
+  },
+  {
+    "revision": "4d2ed92feced8eb26814d0d872c0ba77",
+    "url": "/designhouse/static/media/h-about.4d2ed92f.png"
+  },
+  {
+    "revision": "20de1fd4fff48c8b1fc3a5a31c94183d",
+    "url": "/designhouse/static/media/contact.20de1fd4.png"
+  },
+  {
+    "revision": "1b5a1c30122d597158ac0d428c1eb42d",
+    "url": "/designhouse/static/media/banner-contact.1b5a1c30.png"
+  },
+  {
+    "revision": "d0bc1d7dae38324582901051d89d6203",
+    "url": "/designhouse/static/media/banner-home.d0bc1d7d.png"
+  },
+  {
+    "revision": "cb8274b4dd78e0ffbdd5f51debb0dd87",
+    "url": "/designhouse/static/media/banner-service.cb8274b4.png"
+  },
+  {
+    "revision": "51241109ab860d376479700bc725e1e0",
+    "url": "/designhouse/static/media/h-blog.51241109.png"
+  },
+  {
+    "revision": "83861c175bf597ab511896b52281f364",
+    "url": "/designhouse/static/media/h-service.83861c17.png"
+  },
+  {
+    "revision": "bc58e54922173d19590ae8ea6a7a1b35",
+    "url": "/designhouse/static/media/h-contact.bc58e549.png"
+  },
+  {
+    "revision": "cdcec2ec05ea42c48e828db40796013d",
+    "url": "/designhouse/static/media/h-work.cdcec2ec.png"
+  },
+  {
+    "revision": "84db5da9d2e10de9a0ebb5e127f9624d",
+    "url": "/designhouse/index.html"
+  }
+];
